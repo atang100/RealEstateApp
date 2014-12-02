1bbaca8eed5f6706e89c01b970ce573d01637f59
atang100 <atang100@uottawa.ca>
2014-12-01 21:44:29 -0500

    Fixed Map usage for CreateUserController

--- grails-app/controllers/seg3102/CreateUserController.groovy
+++ grails-app/controllers/seg3102/CreateUserController.groovy
@@ -3,25 +3,25 @@ import seg3102.AgentService
 
 class CreateUserController {
 
+    def CustomerService
+    def OwnerService
+    def AgentService
+
     def index() {}
 
     def addUser(){
         def type = params.userType
+        Map map = [:];
         if(type == "Agent"){
-            AgentService agentService = new AgentService();
-            Map map = new HashMap();
             map.givenName = params.givenName
             map.lastName = params.lastName
             map.userName = params.username
             map.password = params.password
             map.creationDate = new Date()
             map.deleted = false
-            System.println(map.toMapString())
-            new AgentService().createAgent(map)//null pointer exception here
+            agentService.createAgent(map)//null pointer exception here
             flash.message = "Agent created."
         }else if(type == "Owner"){
-            OwnerService ownerService = new OwnerService();
-            Map map = new HashMap();
             map.givenName = params.givenName
             map.lastName = params.lastName
             map.userName = params.username
@@ -31,8 +31,6 @@ class CreateUserController {
             ownerService.createOwner(map)//null pointer exception here
             flash.message = "Owner created."
         }else if(type == "Customer"){
-            CustomerService customerService = new CustomerService();
-            Map map = new HashMap();
             map.givenName = params.givenName
             map.lastName = params.lastName
             map.userName = params.username
